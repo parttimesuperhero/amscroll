682c34155bcb6e930bb6a51409888db980182311
Matt Hollis <hollimat@IOM5006630.local>
2017-03-29 12:49:05 +0100

    Fixing dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 const browserify = require('gulp-browserify');
-const babelify = require('babelify');
 const rename = require("gulp-rename");
 
 gulp.task('default', () => {
@@ -14,17 +14,6 @@ gulp.task('default', () => {
 });
 
 
-// gulp.task('demo', () => {
-//   return gulp.src('demo/app.js')
-//     .pipe(browserify({
-//         transform: ['babelify'],
-//         insertGlobals : true
-//       }))
-//     .pipe(rename("app.min.js"))
-//     .pipe(gulp.dest('demo'));
-// });
-
-
 gulp.task('demo', function () {
   gulp.src('./demo/app.js', { read: false })
     .pipe(browserify({
@@ -33,12 +22,3 @@ gulp.task('demo', function () {
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('./demo/'))
 });
-
-
-  // gulp.src('src/coffee/app.coffee', { read: false })
-  //   .pipe(browserify({
-  //     transform: ['coffeeify'],
-  //     extensions: ['.coffee']
-  //   }))
-  //   .pipe(rename('app.js'))
-  //   .pipe(gulp.dest('./build/js'))
